e706ecbca2c709ab0a70db9c587781a1f62ef21a
NanciuDavid <nanciualexandrudavid22@stud.ase.ro>
2025-05-08 08:46:30 +0300

    Added Private Routing feature

---
+++ src/components/routing/PrivateRoute.tsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+
+interface PrivateRouteProps {
+  isAuthenticated: boolean;
+}
+
+const PrivateRoute = ({ isAuthenticated }: PrivateRouteProps) => {
+  const location = useLocation();
+  const publicPaths = ['/home', '/about-us', '/'];
+
+
+  if (!isAuthenticated && !publicPaths.includes(location.pathname)) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default PrivateRoute;
