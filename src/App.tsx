e291f89903d76d478cbd33d02bc1bbd8c551f09b
NanciuDavid <nanciualexandrudavid22@stud.ase.ro>
2025-05-08 08:48:09 +0300

    Refactor App component to include routing for Login, Register, About, and Property listings; add new NotFound page and Memoji assets.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Footer from './components/layout/Footer'
-import Header from './components/layout/Header' 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PropertyCard from './components/properties/PropertyCard';
 import Layout from './components/layout/Layout';
-import {mockProperties} from './data/mockdata';
+import { mockProperties } from './data/mockdata';
 import HomePage from './pages/HomePage';
-import HeroSearch from './components/home/HeroSearch';
+import AboutPage from './pages/AboutUsPage';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import PrivateRoute from './components/routing/PrivateRoute';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
+
   return (
     <Router>
-    <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Layout>
-  </Router>
+      <Layout>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          
+          {/* Private Routes */}
+          <Route element={<PrivateRoute isAuthenticated={true}/>}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about-us" element={<AboutPage />} />
+            <Route path="/new-listings" element={<PropertyCard property={mockProperties[0]} />} />
+            <Route path='/price-prediciton' element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
